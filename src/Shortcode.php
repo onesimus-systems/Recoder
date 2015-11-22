b793aca6cdaafb32db3cece51b4e42aa47028cb6
Lee Keitel <keitellf@gmail.com>
2015-11-22 08:53:35 -0600

    Initial Commit

---
+++ src/Shortcode.php
@@ -0,0 +1,130 @@
+<?php
+namespace Shortcode;
+
+class Shortcode
+{
+    private $opening = '[';
+    private $closing = ']';
+    private $codes = [];
+
+    public function __construct($open=null, $close=null)
+    {
+        $open = $open ?: $this->opening;
+        $close = $close ?: $this->closing;
+        $this->setDelimiters($open, $close);
+    }
+
+    public function setDelimiters($open, $close=null)
+    {
+        $this->opening = $open;
+        $this->closing = is_null($close) ? $open : $close;
+    }
+
+    public function process()
+    {
+        $args = func_get_args();
+        $text = array_shift($args);
+        while(true) {
+            $sc = $this->getShortCodes($text);
+            if (empty($sc)) {
+                break;
+            }
+            $text = $this->processCodes($text, $sc, $args);
+        }
+        return $text;
+    }
+
+    public function register($code, callable $func)
+    {
+        $this->codes[$code] = $func;
+    }
+
+    public function registerAlias($alias, $code)
+    {
+        $this->codes[$alias] = "_$code";
+    }
+
+    public function unregister($code = null)
+    {
+        if ($code === null) {
+            $this->codes = [];
+            return;
+        }
+
+        if (array_key_exists($code, $this->codes)) {
+            unset($this->codes[$code]);
+        }
+    }
+
+    protected function getShortCodes($text)
+    {
+        $opening = preg_quote($this->opening, '~');
+        $closing = preg_quote($this->closing, '~');
+        $shortcodeRegex = "~$opening([\w-]*)?((?:\s(?:[\w-]*)(?:=(?:\".*?\"|\S+))?)*)$closing(?:(.*?)$opening\/\\1$closing)?~";
+        $optionsRegex = "~(?:\s([\w-]*)(?:=(?:\"(.*?)\"|(\S+)))?)~";
+
+        if (!preg_match_all($shortcodeRegex, $text, $matches, PREG_SET_ORDER | PREG_OFFSET_CAPTURE)) {
+            return [];
+        }
+
+        $codes = [];
+        foreach($matches as $match) {
+            if ($this->getCodeCallable($match[1][0]) === null) {
+                continue;
+            }
+            preg_match_all($optionsRegex, $match[2][0], $matches2, PREG_SET_ORDER);
+
+            $options = [
+                '_raw' => $match[0][0],
+                '_code' => $match[1][0],
+                '_offset' => $match[0][1],
+                '_length' => mb_strlen($match[0][0]),
+                '_content' => isset($match[3]) ? $match[3][0] : '',
+            ];
+            foreach($matches2 as $option) {
+                if (count($option) === 2) {
+                    $options[$option[1]] = true;
+                } else{
+                    $options[$option[1]] = $option[2] ?: $option[3];
+                }
+            }
+            $codes []= $options;
+        }
+        return $codes;
+    }
+
+    protected function processCodes($text, $codes, $args)
+    {
+        $offsetCorrection = 0;
+        foreach($codes as $code) {
+            $sc = $code['_code'];
+            array_unshift($args, $code);
+            $func = $this->getCodeCallable($sc);
+            if ($func === null) {
+                continue;
+            }
+
+            $replacement = call_user_func_array($func, $args);
+            if ($replacement !== null) {
+                $offset = $code['_offset']+$offsetCorrection;
+                $text = mb_substr($text, 0, $offset).$replacement.mb_substr($text, $offset+$code['_length']);
+                $offsetCorrection += mb_strlen($replacement) - mb_strlen($code['_raw']);
+            }
+        }
+        return $text;
+    }
+
+    protected function getCodeCallable($code)
+    {
+        if (!array_key_exists($code, $this->codes)) {
+            return null;
+        }
+
+        $func = $this->codes[$code];
+        if (is_string($func)) {
+            $aliased = substr($func, 1);
+            return $this->getCodeCallable($aliased);
+        }
+        return $func;
+    }
+}
---
+++ tests/ShortcodeTest.php
@@ -0,0 +1,61 @@
+<?php
+namespace Shortcode;
+
+class ShortcodeTests extends \PHPUnit_Framework_TestCase
+{
+    public static function getShortcodeProcessor()
+    {
+        $sc = new Shortcode();
+        $sc->register('name', function($options) {
+            return $options['_code'];
+        });
+        $sc->register('content', function($options) {
+            return $options['_content'];
+        });
+        $sc->register('reverse', function($options) {
+            return strrev($options['_content']);
+        });
+        $sc->register('url', function($options) {
+            if ($options['_content'] !== '') {
+                return '<a href="'.$options['_content'].'">'.$options['_content'].'</a>';
+            }
+            return '<a href="'.$options['url'].'">'.$options['url'].'</a>';
+        });
+        $sc->registerAlias('c', 'content');
+        $sc->registerAlias('n', 'name');
+        return $sc;
+    }
+
+    public function caseProvider()
+    {
+        return [
+            ['[name]', 'name'],
+            ['[content]random[/content]', 'random'],
+            ['[content]象形字[/content]', '象形字'],
+            ['xxx [content]象形字[/content] yyy', 'xxx 象形字 yyy'],
+            ['xxx [content]ąćęłńóśżź ąćęłńóśżź[/content] yyy', 'xxx ąćęłńóśżź ąćęłńóśżź yyy'],
+            ['[name]random[/other]', 'namerandom[/other]'],
+            ['[name][other]random[/other]', 'name[other]random[/other]'],
+            ['[content]random-[name]-random[/content]', 'random-name-random'],
+            ['random [content]other[/content] various', 'random other various'],
+            ['x [content]a-[name]-b[/content] y', 'x a-name-b y'],
+            ['x [c]a-[n][/n]-b[/c] y', 'x a-n-b y'],
+            ['x [content]a-[c]v[/c]-b[/content] y', 'x a-v-b y'],
+            ['x [html]bold[/html] z', 'x [html]bold[/html] z'],
+            ['x [reverse]abc xyz[/reverse] z', 'x zyx cba z'],
+            ['x [i /][i]i[/i][i /][i]i[/i][i /] z', 'x [i /][i]i[/i][i /][i]i[/i][i /] z'],
+            ['x [url url="http://giggle.com/search"] z', 'x <a href="http://giggle.com/search">http://giggle.com/search</a> z'],
+            ['x [url="http://giggle.com/search"] z', 'x [url="http://giggle.com/search"] z'],
+            ['x [url]http://giggle.com/search[/url] z', 'x <a href="http://giggle.com/search">http://giggle.com/search</a> z'],
+        ];
+    }
+
+    /**
+     * @dataProvider caseProvider
+     */
+    public function testContent($text, $expected)
+    {
+        $sc = $this->getShortcodeProcessor();
+        $this->assertEquals($expected, $sc->process($text));
+    }
+}
